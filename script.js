f0688c03605cbf821f36db61e862a0850c7ce0dc
a-sowa <axelsowapro@gmail.com>
2024-04-19 21:34:33 +0200

    Add arrays to store messsages components

---
+++ script.js
@@ -0,0 +1,4 @@
+// Messages components
+const activitiesIdeas = ['Read a book', 'Listen to music', 'Take care of you', 'Call a friend', 'Learn something new'];
+const motivations = ['pleasant', 'cool', 'funny', 'stimulating', 'exciting', 'rewarding', 'comforting'];
+const momentsSuggestions = ['at the end of the day', 'during the morning', 'in the evening', 'when it\'s most convenient for you']
