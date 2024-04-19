7849499c897deabd44c702231573ca5ad48be033
a-sowa <axelsowapro@gmail.com>
2024-04-19 21:50:50 +0200

    Add function to generate random index

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 // Messages components
 const activitiesIdeas = ['Read a book', 'Listen to music', 'Take care of you', 'Call a friend', 'Learn something new'];
 const motivations = ['pleasant', 'cool', 'funny', 'stimulating', 'exciting', 'rewarding', 'comforting'];
-const momentsSuggestions = ['at the end of the day', 'during the morning', 'in the evening', 'when it\'s most convenient for you']
+const momentsSuggestions = ['at the end of the day', 'during the morning', 'in the evening', 'when it\'s most convenient for you'];
+
+// Generates random index for messages
+const getRandomIndex = arr => Math.floor(Math.random() * arr.length);
+
